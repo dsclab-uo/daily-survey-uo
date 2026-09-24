3d4a59e2fc5cf042a56703d88e0a87fc03d70e4f
dsclab-uo <dsclab@uoregon.edu>
2026-09-23 20:15:09 -0700

    Update config.js

--- config.js
+++ config.js
@@ -11,14 +11,14 @@ const CONFIG = {
   STUDY_LENGTH_DAYS: 30,
 
   // Times of day surveys are triggered (24h "HH:MM", local time).
-  SURVEY_TIMES: ["12:00", "20:00"],
+  SURVEY_TIMES: ["20:20", "20:25"],
 
   // How many times a snoozed notification will re-fire, and how
   // far apart (minutes), before it stops repeating for that occasion.
   SNOOZE_REPEAT_MAX: 3,
-  SNOOZE_INTERVAL_MINUTES: 60,
+  SNOOZE_INTERVAL_MINUTES: 1,
 
   // How often (ms) the open app re-checks whether a notification
   // is due. 60000 = once a minute.
-  CHECK_INTERVAL_MS: 60000
+  CHECK_INTERVAL_MS: 60
 };
